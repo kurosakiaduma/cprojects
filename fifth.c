108f087efd3da169605652d5a42c347fe51f838a
Tevin Joel Aduma <tevin74@live.com>
2017-12-12 23:12:34 +0300

    set condition to print a statement when parameters on interger data type are met||used else for second parameter

--- fifth.c
+++ fifth.c
@@ -9,12 +9,21 @@ int main()
 	if(expression){
 					statements;					
 					}
+	else{
+		 statements; 	
+	}
+
 	*/
 	
 	int age = 10;
 	if (age <= 18) {
 
 		printf("Okay, you are not it fam!");
+	}
+	else{
+	
+		printf("You made it fam");
+
 	}
 
 	_getch();
