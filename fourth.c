2187d6a2b0ab18f0f6fb8207700e984f5e0a9c2d
Tevin Joel Aduma <tevin74@live.com>
2017-12-12 19:03:45 +0300

    code cleaned:build successful

--- fourth.c
+++ fourth.c
@@ -6,11 +6,13 @@ int main()
 	/*scanf("format string", list of address of variables);*/
 
 	int age;
+	float weight;
 
-	printf("Enter your age here \n");
 
-	scanf_s("%d", &age);
-	printf("Your age is %d", age);
+	printf("Enter your age and weight here \n");
+
+	scanf_s("%d %f", &age, &weight);
+	printf("Your age is %d \n Your weight is %f", age, weight);
 
 	_getch();
 	return 0;
