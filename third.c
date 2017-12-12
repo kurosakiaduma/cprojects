68b998ecc671a6bea0f6f095b4218ab6a3c18bac
Tevin Joel Aduma <tevin74@live.com>
2017-12-12 18:10:17 +0300

    code cleaned:build successful

--- third.c
+++ third.c
@@ -4,10 +4,12 @@
 int main()
 {
 	int age = 21;
+	float weight = 55.8;
+	char sex = "boy";
 
 	/*printf ("format string", list of variables); */
 	
-	printf("my name is %d",age);
+	printf("my name is %d \n I weigh %f kg \n I am a black %c",age,weight,sex);
 
 	_getch();
 	
