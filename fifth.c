70695f1495dd736bbadfe031dc18e5ae32b3aea2
Tevin Joel Aduma <tevin74@live.com>
2017-12-12 22:55:53 +0300

    set condition to print a statement when parameters on interger data type are met

---
+++ fifth.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+
+	/*
+	
+	if(expression){
+					statements;					
+					}
+	*/
+	
+	int age = 20;
+	if (age >= 18) {
+
+		printf("Okay, you me it fam!");
+	}
+
+	_getch();
+	return 0;
+}
