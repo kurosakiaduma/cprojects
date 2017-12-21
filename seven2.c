2b1043b744c47f42f936449ca2889d50073295d8
Tevin Joel Aduma <tevin74@live.com>
2017-12-21 22:48:26 +0300

    print value of x as 10

---
+++ seven2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main() {
+
+
+	/*
+	
+	++ increment by value operator
+
+	-- decrement by value operator
+	
+	*/
+
+	int x = 10;
+	printf("X value is %d\n",x);
+	
+	_getch();
+	return 0;
+}
