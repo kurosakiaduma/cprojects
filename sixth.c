73ad2a2c43c479610ac6b284b17f3ce538eaccc0
Tevin Joel Aduma <tevin74@live.com>
2017-12-21 21:33:21 +0300

    created two interger variables and performed addition operations on them

---
+++ sixth.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+int main()){
+
+/*
+
++ for addition operations
+/ for division operations
+* for multiplication
+% mod for mod operation
+
+++ increment
+-- decerement
+
+*/
+
+int num1, num2, result;
+
+num1 = 20;
+num2 - 10;
+
+result = num1 + num2;
+
+printf("$d", result);
+
+_getch();
+
+return 0;
+}
