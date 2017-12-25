0f79deca523c63facd7924cb87f498f1e0cdf7c1
Tevin Joel Aduma <tevin74@live.com>
2017-12-25 21:00:22 +0300

    do loop C source code||int set to an increment based on a set condition

--- C Loops/C Loops/Do Loop.cpp
+++ C Loops/C Loops/Do Loop.cpp
@@ -2,6 +2,15 @@
 #include<conio.h>
 
 int main() {
+	/*local variable definition*/
+	int a = 10;
+
+	/*do loop execution*/
+	do
+	{
+		printf("value of a is %d\n",a);
+		a++;
+	} while (a < 20);
 
 	_getch();
 	return 0;
