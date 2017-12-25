c3341e0dfa69b1f36ed551192ac8e5d9f9ffaf5d
Tevin Joel Aduma <tevin74@live.com>
2017-12-25 20:24:54 +0300

    sample code for nested switch

--- NestedSwitch/NestedSwitch/NestedSwitch.cpp
+++ NestedSwitch/NestedSwitch/NestedSwitch.cpp
@@ -3,6 +3,24 @@
 
 int main() {
 
+	/*local variable definition*/
+
+	const a = 100;
+	const b = 200;
+
+	switch (a)
+	{
+	case 100:
+		printf("This is part of outer switch\n", a);
+		switch (b)
+		{
+		case 200:
+			printf("This is part of inner switch\n",a);
+		}
+	}
+
+	printf("Ëxact value of a is %d\n", a);
+	printf("Exact value of b is %d\n", b);
 
 
 	_getch();
