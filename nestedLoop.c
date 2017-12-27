a6ec979615a0e0ebd775a51c5436c4be2b387d4e
Tevin Joel Aduma <tevin74@live.com>
2017-12-27 15:07:59 +0300

    set the condition for test of prime numbers using conditional operator for th divisbility of i and j

--- nestedLoop.c
+++ nestedLoop.c
@@ -7,16 +7,12 @@ int main()
 	/*locl variable  definition*/
 	int i, j;
 
-	for (i = 2; i < 100; i++);
-	{
+	for (i = 2; i < 100; i++) {
 
-		for (j = 2; j < 100; j++);
-		{
+		for (j = 2; j < 100; j++)
+
 			if (!(i%j)) break; //if factor found, not prime
-			{
-
-			}
-		}
+		if (j > (i / j)) printf("%d is a prime number\n", i);
 	}
 
 
