edc7ba6aba3ff7f12e4bf4dca2e5a598783997ce
Tevin Joel Aduma <tevin74@live.com>
2018-01-08 00:44:08 +0300

    diff tut worked on in brk project

---
+++ breakStatement/breakStatement/breakStatement.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main() {
+
+	int regress;
+	int p;
+	int g;
+
+
+	p = 30;
+	regress = 50;
+
+	g = p + regress;
+
+	printf("%d", g);
+
+	_getch();
+	return 0;
+}
